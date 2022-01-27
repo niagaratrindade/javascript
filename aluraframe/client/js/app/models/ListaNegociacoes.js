4a8f020edf4d41bbe54732b1edef0f0e6b62d9f5
Nia <niagara.trindade@gmail.com>
2022-01-27 11:46:49 -0300

    Adicionando o Reflect.apply para modificar o contexto this de ListaNegociacoes para NegociacaoController

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,13 +1,14 @@
 class ListaNegociacoes{
 
-    constructor(armadilha){
+    constructor(contexto, armadilha){
         this._negociacoes = []
         this._armadilha = armadilha;
+        this._contexto = contexto;
     }
 
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
+        Reflect.apply(this._armadilha, this._contexto, [this])
     }
     
     get negociacoes() {
@@ -16,6 +17,6 @@ class ListaNegociacoes{
 
     esvazia() {
         this._negociacoes = [];
-        this._armadilha(this);
+        Reflect.apply(this._armadilha, this._contexto, [this])
     }
 }
