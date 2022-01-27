9e67d0b3d620f440ecde9c5be9e2bb0c8f561ef9
Nia <niagara.trindade@gmail.com>
2022-01-27 11:55:40 -0300

    Eliminando o contexto e o Reflect.apply e modificando a função de atualizar a view por uma arrow function no NegociacaoController para alterar o this de dinâmico para lexico e assim simplificar o código

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,13 @@
 class ListaNegociacoes{
 
-    constructor(contexto, armadilha){
+    constructor(armadilha){
         this._negociacoes = []
         this._armadilha = armadilha;
-        this._contexto = contexto;
     }
 
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
-        Reflect.apply(this._armadilha, this._contexto, [this])
+        this._armadilha(this);
     }
     
     get negociacoes() {
@@ -17,6 +16,6 @@ class ListaNegociacoes{
 
     esvazia() {
         this._negociacoes = [];
-        Reflect.apply(this._armadilha, this._contexto, [this])
+        this._armadilha(this);
     }
 }
