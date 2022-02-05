db999976ef36ecd890c2d03029d939f8abfc7a4d
Nia <niagara.trindade@gmail.com>
2022-02-05 19:22:04 -0300

    Adicionando função importa negociações para importar documentos com negociações já presente nos arquivos do PC

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -25,6 +25,26 @@ class NegociacaoController {
         this._limpaFormulario();
     }
 
+    importaNegociacoes() {
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'negociacoes/semana');
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4) {
+                if(xhr.status == 200) {
+                    JSON.parse(xhr.responseText)
+                    .map(objeto => new Negociacao(new Date(objeto.data),objeto.quantidade,objeto.valor))
+                    .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto = 'Negociações importadas com sucesso'
+                } else {
+                    console.log(xhr.responseText)
+                    this._mensagem.texto = 'Não foi possível obter as negociações da semana.'
+                }
+            }
+        }
+        xhr.send();
+    }
+
     _criaNegociacao() {
         return new Negociacao(
             DataHelper.textoparaData(this._inputData.value),
